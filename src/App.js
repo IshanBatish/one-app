d30dedffbcecac2a9592a32f6f0a3bcac0e9e488
Ishan Batish <71932861+IshanBatish@users.noreply.github.com>
2021-09-09 10:02:10 -0400

    Init File Master

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavigationBar from './NavigationBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> This is our HomePage</h1>
+        <NavigationBar />
       </header>
     </div>
   );
---
+++ src/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './NavigationBar.css';
+
+
+function NavigationBar(){
+    return(
+      <div className="NavBar">
+      <ul>
+        <li>HomePage</li>
+        <li>OfficePage</li>
+        <li>CountryPage</li>
+      </ul>
+      </div>
+    )
+  }
+  
+export default NavigationBar;
